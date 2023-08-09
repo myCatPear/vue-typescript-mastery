ad30a13d2fe109edc9e12249ceeb38254163dfa0
Денис Тавабилов <dagger1233@gmail.com>
2023-08-09 07:44:37 +0500

    pinia

--- app/src/views/RestaurantsPage.vue
+++ app/src/views/RestaurantsPage.vue
@@ -5,6 +5,7 @@ import NewRestaurantForm from '../components/NewRestaurantForm.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import SideMenu from '../components/SideMenu.vue'
 import type { Restaurant } from '@/types'
+import { useReustaurantStore } from '@/store/RestaurantStore'
 
 type DataShape = {
   filterText: string
@@ -12,39 +13,20 @@ type DataShape = {
   showNewForm: boolean
 }
 
+const reustaurantStore = useReustaurantStore()
+
+const reustaurantList = reustaurantStore.list
+
 const filterText = ref('')
-const restaurantList = ref<Restaurant[]>([
-  {
-    id: '9f995ce4-d2fc-4d00-af1d-6cb1647c6bd3',
-    name: 'Quiche From a Rose',
-    address: '283 Thisisnota St.',
-    website: 'www.quichefromarose.com',
-    status: 'Want to Try',
-  },
-  {
-    id: 'ae62a3da-791b-4f44-99a1-4be1b0ec30b8',
-    name: 'Tamago Never Dies',
-    address: '529 Letsgofora Dr.',
-    website: 'www.tamagoneverdies.com',
-    status: 'Recommended',
-  },
-  {
-    id: '9b361dae-2d44-4499-9940-97e188d41a32',
-    name: 'Penne For Your Thoughts',
-    address: '870 Thisisa St.',
-    website: 'www.penneforyourthoughts.com',
-    status: 'Do Not Recommend',
-  },
-])
 
 const showNewForm = ref(false)
 
 const filteredRestaurantList = computed((): Restaurant[] => {
-  return restaurantList.value.filter((restaurant) => {
+  return reustaurantList.filter((restaurant) => {
     if (restaurant.name) {
       return restaurant.name.toLowerCase().includes(filterText.value.toLowerCase())
     } else {
-      return restaurantList
+      return reustaurantList
     }
   })
 })
@@ -52,17 +34,6 @@ const filteredRestaurantList = computed((): Restaurant[] => {
 const numberOfRestaurants = computed(() => {
   return filteredRestaurantList.value.length
 })
-
-const addRestaurant = (payload: Restaurant) => {
-  restaurantList.value.push(payload)
-  hideForm()
-}
-
-const deleteRestaurant = (payload: Restaurant) => {
-  restaurantList.value = restaurantList.value.filter((restaurant) => {
-    return restaurant.id !== payload.id
-  })
-}
 
 const hideForm = () => {
   showNewForm.value = false
@@ -114,12 +85,16 @@ onMounted(() => {
         </nav>
 
         <!-- New Restaurant Form -->
-        <NewRestaurantForm v-if="showNewForm" @add-new-restaurant="addRestaurant" @cancel-new-restaurant="hideForm" />
+        <NewRestaurantForm
+          v-if="showNewForm"
+          @add-new-restaurant="reustaurantStore.addRestaurant"
+          @cancel-new-restaurant="hideForm"
+        />
 
         <!-- Display Results -->
         <div v-else class="columns is-multiline">
           <div v-for="item in filteredRestaurantList" class="column is-full" :key="`item-${item}`">
-            <RestaurantCard :restaurant="item" @delete-restaurant="deleteRestaurant" />
+            <RestaurantCard :restaurant="item" @delete-restaurant="reustaurantStore.deleteRestaurant" />
           </div>
         </div>
       </div>
